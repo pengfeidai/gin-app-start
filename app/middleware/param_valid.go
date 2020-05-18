a75464751975ccff824e4d5542bcf978fc714e74
pengfeidai <302804389@qq.com>
2020-05-18 21:56:02 +0800

    feat: add validate bind

--- app/middleware/param_valid.go
+++ app/middleware/param_valid.go
@@ -1,13 +1,38 @@
 package middleware
 
+// ShouldBind
 func (c *Context) Validate(p interface{}) error {
-	// validata := validator.New()
-	// if err := validata.Struct(p); err != nil {
-	// 	log.Println("param validate err:", err)
-	// 	c.Response(err.Error(), nil)
-	// 	return err
-	// }
 	if err := c.Ctx.ShouldBind(p); err != nil {
+		logger.Error("param validate error:", err)
+		c.Response(400, err.Error(), nil)
+		return err
+	}
+	return nil
+}
+
+// ShouldBindUri
+func (c *Context) ValidateRouter(p interface{}) error {
+	if err := c.Ctx.ShouldBindUri(p); err != nil {
+		logger.Error("param validate err:", err)
+		c.Response(400, err.Error(), nil)
+		return err
+	}
+	return nil
+}
+
+// ShouldBindQuery
+func (c *Context) ValidateQuery(p interface{}) error {
+	if err := c.Ctx.ShouldBindQuery(p); err != nil {
+		logger.Error("param validate err:", err)
+		c.Response(400, err.Error(), nil)
+		return err
+	}
+	return nil
+}
+
+// ShouldBindJSON
+func (c *Context) ValidateJSON(p interface{}) error {
+	if err := c.Ctx.ShouldBindJSON(p); err != nil {
 		logger.Error("param validate err:", err)
 		c.Response(400, err.Error(), nil)
 		return err
