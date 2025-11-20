5bf01ddd201252ca77f2cfcb3514cf7047b389dd
fly <302804389@qq.com>
2025-11-20 14:42:19 +0800

    chore: remove deprecated configuration and main files

--- app/schema/health_check.go
+++
@@ -1,5 +0,0 @@
-package schema
-
-type Health struct {
-	Name string `form:"name" validate:"required,min=1,max=32"`
-}
--- app/schema/user.go
+++
@@ -1,6 +0,0 @@
-package schema
-
-type User struct {
-	Name     string `form:"name" json:"name" binding:"required"`
-	Password string `form:"password" json:"password" binding:"required"`
-}
